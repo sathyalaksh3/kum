ae409e63d6917a23f80f55221a3ca94f0873fc85
sathyalaksh3 <30349849+sathyalaksh3@users.noreply.github.com>
2017-07-30 15:11:47 +0530

    Create kumaravel.c

---
+++ kumaravel.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char s;
+printf("enter a character:");
+scanf("%c",&s);
+if(s=='a'&&s=='z'||s=='A'&&s=='Z')
+printf("alphabet");
+else
+printf("not alphabet");
+return 0;
+}
