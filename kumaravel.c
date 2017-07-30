0c35e8fb5a10f6f34f0a15e4a511a68c9361d99b
sathyalaksh3 <30349849+sathyalaksh3@users.noreply.github.com>
2017-07-30 15:17:58 +0530

    Update kumaravel.c

--- kumaravel.c
+++ kumaravel.c
@@ -4,7 +4,7 @@ int main()
 char s;
 printf("enter a character:");
 scanf("%c",&s);
-if(s=='a'&&s=='z'||s=='A'&&s=='Z')
+if(s>='a'&&s<='z'||s>='A'&&s<='Z')
 printf("alphabet");
 else
 printf("not alphabet");
